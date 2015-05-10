f847198201fcc06f8c28dce79758e6b78c81fbbc
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2015-05-10 20:21:19 +0530

    Sometimes all you need is a little hit in the head, but this was a big one to be fair! Finally added flymake-mode for Python.

--- init.el
+++ init.el
@@ -265,6 +265,20 @@ inhibit-startup-echo-area-message t)
                               (hack-local-variables)
                               (venv-workon project-venv-name)))
 
+;; Flymake for Python.
+;; Needs Pyflakes - http://pypi.python.org/pypi/pyflakes
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                       'flymake-create-temp-inplace))
+           (local-file (file-relative-name
+                        temp-file
+                        (file-name-directory buffer-file-name))))
+      (list "pyflakes" (list local-file))))
+  (add-to-list 'flymake-allowed-file-name-masks
+               '("\\.py\\'" flymake-pyflakes-init)))
+(add-hook 'python-mode-hook 'flymake-mode)
+
 ;; jedi config
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:complete-on-dot t)
