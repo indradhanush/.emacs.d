a8dc88125590ae3e99c07b2e57a3361bbf7c07fd
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2015-11-20 23:26:36 +0530

    Custom face changes for mode line and which-function-mode face changes.

--- init.el
+++ init.el
@@ -162,7 +162,9 @@ inhibit-startup-echo-area-message t)
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:background "#504945" :foreground "#A89984" :box nil))))
+ '(mode-line-inactive ((t (:inherit mode-line :background "#1D2021" :foreground "#EBDBB2" :box nil :weight light))))
+ '(which-func ((t (:foreground "sienna3")))))
 
 ;; Desktop Mode
 ;; (desktop-save-mode 1)
