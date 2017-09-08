82131d9ef8695044ab758d3bcc5c43a44623e8ff
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2017-09-08 10:18:19 +0530

    Better indentation for erlang mode

--- config/erlang.el
+++ config/erlang.el
@@ -6,9 +6,13 @@
 
 (setq-default erlang-root-dir "/usr/local/bin")
 
+
+(add-hook 'erlang-mode-hook (lambda () (electric-indent-local-mode -1)))
+
 (add-hook 'erlang-mode-hook 'erlang-setup
           (defun erlang-setup ()
             'auto-complete-mode 1
-            ))
+            (setq-default erlang-electric-semicolon-insert-blank-lines 1)
+            (setq-default erlang-argument-indent 4)))
 
 ;;; erlang.el ends here
