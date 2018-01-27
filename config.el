fce0c05d057d0fe930be8e7c9578fb9b33f29daf
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2018-01-27 10:53:16 +0100

    emc-merge-config-files

--- config.el
+++ config.el
@@ -169,6 +169,18 @@
 
 (toggle-truncate-lines 1)
 
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)
+        (forward-line)))
+
+(global-set-key (kbd "M-;") 'comment-or-uncomment-region-or-line)
+
 (provide 'editor)
 ;;; editor.el ends here
 ;; ############################################################################
