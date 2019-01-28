e30764d20c4112eba5a55ddbb0f07912e84e6128
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2019-01-28 14:20:12 +0530

    lisp: Update custom variables

--- lisp/custom.el
+++ lisp/custom.el
@@ -4,18 +4,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
-   ["#3C3836" "#FB4934" "#84BB26" "#FABD2F" "#83A598" "#D3869B" "#3FD7E5" "#EBDBB2"])
+   (vector "#ffffff" "#f36c60" "#8bc34a" "#fff59d" "#4dd0e1" "#b39ddb" "#81d4fa" "#263238"))
  '(blink-cursor-mode nil)
  '(custom-safe-themes
    (quote
-    ("5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "5376e3af02f5614851da9633e7a272b6c8824c9a79e7c0fc2bbc7d6f2aaaf009" "bbf759c7abc48267b9cb2dd5273eb08d968a45d9eb76abdd2959ef2cf98b3397" default)))
+    ("732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "5376e3af02f5614851da9633e7a272b6c8824c9a79e7c0fc2bbc7d6f2aaaf009" "bbf759c7abc48267b9cb2dd5273eb08d968a45d9eb76abdd2959ef2cf98b3397" default)))
  '(echo-bell-mode nil)
  '(flycheck-flake8-maximum-line-length nil)
  '(global-auto-revert-mode t)
  '(helm-mode t)
  '(package-selected-packages
    (quote
-    (floobits rotate edts auto-complete-auctex auctex magit ggtags helm-gtags intero llvm-mode ledger-mode flycheck-tip erlang helm-flyspell ac-ispell flyspell-correct keyfreq ivy-hydra nginx-mode vlf yaml-mode auto-highlight-symbol sql-indent php-mode dockerfile-mode pip-requirements ag helm-ag ac-dabbrev go-guru golint go-autocomplete go-eldoc fill-column-indicator fci markdown-mode counsel counsel-git-grep ssh-config-mode go-mode swiper virtualenvwrapper exec-path-from-shell virtualenv jedi ace-jump-mode helm-swoop find-file-in-repository smex find-file-in-respository helm-flycheck material-theme helm-config helm yafolding smartparens company darktooth-theme smexz smexh darktooth restart-emacs)))
+    (fic-mode elpy terraform-mode python-mode multiple-cursors go-playground floobits rotate edts auto-complete-auctex auctex magit ggtags helm-gtags intero llvm-mode ledger-mode flycheck-tip erlang helm-flyspell ac-ispell flyspell-correct keyfreq ivy-hydra nginx-mode vlf yaml-mode auto-highlight-symbol sql-indent php-mode dockerfile-mode pip-requirements ag helm-ag ac-dabbrev go-guru golint go-autocomplete go-eldoc fill-column-indicator fci markdown-mode counsel counsel-git-grep ssh-config-mode go-mode swiper virtualenvwrapper exec-path-from-shell virtualenv jedi ace-jump-mode helm-swoop find-file-in-repository smex find-file-in-respository helm-flycheck material-theme helm-config helm yafolding smartparens company darktooth-theme smexz smexh darktooth restart-emacs)))
  '(pos-tip-background-color "#36473A")
  '(pos-tip-foreground-color "#FFFFC8")
  '(safe-local-variable-values
