1df73e1a7767b5aed052d87b389e363f7dbfe6a1
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2019-03-21 04:15:03 +0530

    elpa: Add byte compiled packages
    This ensure that I have reproducible emacs setups. Often when setting
    up a emacs on a new system, a lot of packages have new releases that
    break backwards compatibility. With this commit, I can use the version
    tracked installed packages instead to start working and then come back
    at one package at a time and fix it.

---
+++ elpa/golint-20180221.2015/golint-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "golint" "20180221.2015" "lint for the Go source code" 'nil :commit "5906bd5c48cd840279ace88b165057fbbd7fb35a" :url "https://github.com/golang/lint")
