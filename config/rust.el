dfd798231b733b1b427bddb5acfa1e43833bbd13
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2019-08-18 20:48:30 +0530

    rust.el: Add configs for Rust

---
+++ config/rust.el
@@ -0,0 +1,28 @@
+;;; rust --- configuration for rust
+
+;;; Commentary:
+
+;;; Code:
+(use-package rust-mode
+  :ensure t
+  :defer t)
+
+(use-package racer
+  :ensure t
+  :defer t
+  :config
+  (require 'rust-mode)
+  (add-hook 'rust-mode-hook #'racer-mode)
+  (add-hook 'racer-mode-hook #'eldoc-mode)
+  (add-hook 'racer-mode-hook #'company-mode)
+  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (define-key rust-mode-map (kbd "C-.") #'racer-find-definition)
+  (define-key rust-mode-map (kbd "C-,") #'pop-tag-mark)
+
+  (define-key rust-mode-map (kbd "M-.") nil)
+  (define-key rust-mode-map (kbd "M-,") nil)
+
+  (setq company-tooltip-align-annotations t))
+
+(provide 'rust)
+;;; rust.el ends here
