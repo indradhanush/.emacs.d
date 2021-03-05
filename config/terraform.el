bb322cb9736e0e05d38eff6fa06d55c7d155ed93
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2021-03-05 21:09:09 +0530

    config/terraform.el: Add keybinding to toggle hiding

--- config/terraform.el
+++ config/terraform.el
@@ -11,6 +11,7 @@
   :config
   (add-hook 'terraform-mode-hook 'terraform-format-on-save-mode)
   (add-hook 'terraform-mode-hook
-            (lambda () (local-set-key (kbd "C-M-\\") 'terraform-format-buffer))))
+            (lambda () (local-set-key (kbd "C-M-\\") 'terraform-format-buffer)))
+  (local-set-key (kbd "<C-return>") 'hs-toggle-hiding))
 
 ;;; terraform.el ends here
