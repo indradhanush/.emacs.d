d65fc6613ea42a07ca9906d2f3a0813949d2e6b3
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2021-03-05 21:05:35 +0530

    elpa: Update packages

---
+++ elpa/protobuf-mode-20170526.1650/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "20170526.1650" "major mode for editing protocol buffers." 'nil :commit "c08b7512cf9d65899e8e84edd02d4335a64b11b0" :keywords '("google" "protobuf" "languages") :authors '(("Alexandre Vassalotti" . "alexandre@peadrop.com")) :maintainer '("Alexandre Vassalotti" . "alexandre@peadrop.com"))
---
+++ elpa/zenburn-theme-2.6/zenburn-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/dhanush/.emacs.d/elpa/zenburn-theme-2.6/zenburn-theme.el  -*- no-byte-compile: t -*-
+(define-package "zenburn-theme" "2.6" "A low contrast color theme for Emacs." 'nil :commit "4c75edfe7862c903b2584266284ed951004f5b62" :authors '(("Bozhidar Batsov" . "bozhidar@batsov.com")) :maintainer '("Bozhidar Batsov" . "bozhidar@batsov.com") :url "http://github.com/bbatsov/zenburn-emacs")
